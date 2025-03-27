5bd37614333d9087bb85fd77ef4ecf658b403a60
Fran <fmartosmuoz@yahoo.es>
2025-03-27 13:20:11 +0100

    Task Dao added

---
+++ app/src/main/java/com/summercompany/tareas/Task.kt
@@ -0,0 +1,18 @@
+package com.summercompany.tareas
+
+class Task (
+    var id: Long,
+    var title: String,
+    var done: Boolean
+){
+
+    companion object{
+        const val TABLE_NAME = "Tasks"
+
+        const val COLUMN_NAME_ID= "id"
+        const val COLUMN_NAME_TITLE = "title"
+        const val COLUMN_NAME_DONE = "done"
+
+    }
+
+}
